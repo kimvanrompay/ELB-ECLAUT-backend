5fc2755df9877e76a7aac52feded53392a155419
Claire-Sophie Lenaerts <claire-sophie.lenaerts@livecadia.com>
2025-06-11 15:30:25 +0200

    fix: change order by column names to snake case

--- libraries/utils/src/string.utils.ts
+++ libraries/utils/src/string.utils.ts
@@ -5,4 +5,11 @@ const toScreamingSnakeCase = (str: string): string => {
 		.toUpperCase();
 };
 
-export {toScreamingSnakeCase};
+const camelToSnakeCase = (str: string): string => {
+	return str
+		.replace(/([a-z])([A-Z])/g, '$1_$2')
+		.replace(/-/g, '_')
+		.toLowerCase();
+};
+
+export {toScreamingSnakeCase, camelToSnakeCase};
