b4479e40c3180af58bad7f666ca41bb77c6b1022
Claire-Sophie Lenaerts <claire-sophie.lenaerts@livecadia.com>
2025-06-18 11:27:57 +0200

    fix: rename has_temp_password property in db

--- libraries/db/scripts/migrations/V20250617111034__add_password_login.sql
+++ libraries/db/scripts/migrations/V20250617111034__add_password_login.sql
@@ -5,7 +5,7 @@ alter table app_user
 alter table app_user
     add column if not exists hashed_password varchar(255) not null default 'PLEASE_CHANGE_ME';
 alter table app_user
-    add column if not exists hasTempPassword boolean default true;
+    add column if not exists has_temp_password boolean default true;
 alter table app_user
     add column if not exists allow_password_login boolean default true;
 
